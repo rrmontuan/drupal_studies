5f3d05a90098e6de7f57faa889f4cdb69fdba679
Ricardo Montuan <ricardo.montuan@hotmail.com>
2019-06-18 18:09:43 -0300

    feat: Added the loggerFactory service in the controller.

--- app/modules/custom/dino_roar/src/Controller/RoarController.php
+++ app/modules/custom/dino_roar/src/Controller/RoarController.php
@@ -6,19 +6,23 @@ use Symfony\Component\HttpFoundation\Response;
 use Drupal\dino_roar\Jurassic\RoarGenerator;
 use Drupal\Core\Controller\ControllerBase;
 use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\Logger\LoggerChannelFactoryInterface;
 
 class RoarController extends ControllerBase
 {
     private $roarGenerator;
 
-    public function __construct(RoarGenerator $roarGenerator)
+    public function __construct(RoarGenerator $roarGenerator, LoggerChannelFactoryInterface $loggerFactory)
     {
         $this->roarGenerator = $roarGenerator;
+        $this->loggerFactory = $loggerFactory;
     }
 
     public function roar($count)
     {
         $roar = $this->roarGenerator->getRoar($count);
+        $this->loggerFactory->get('default')
+            ->debug($roar);
 
         return new Response($roar);
     }
@@ -26,7 +30,8 @@ class RoarController extends ControllerBase
     public static function create(ContainerInterface $container)
     {
         $roarGenerator = $container->get('dino_roar.roar_generator');
+        $loggerFactory = $container->get('logger.factory');
 
-        return new static($roarGenerator);
+        return new static($roarGenerator, $loggerFactory);
     }
 }
