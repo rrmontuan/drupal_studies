6341bf530149262d610329464eb173786f5f29d5
Ricardo Montuan <ricardo.montuan@hotmail.com>
2019-06-18 18:16:55 -0300

    chore: Added keyvalue Service in RoarGenerator using DI.

--- app/modules/custom/dino_roar/src/Jurassic/RoarGenerator.php
+++ app/modules/custom/dino_roar/src/Jurassic/RoarGenerator.php
@@ -2,10 +2,33 @@
 
 namespace Drupal\dino_roar\Jurassic;
 
+use Drupal\Core\KeyValueStore\KeyValueFactoryInterface;
+
 class RoarGenerator 
 {
+    private $keyValueFactory;
+
+    public function __construct(KeyValueFactoryInterface $keyValueFactory)
+    {
+        $this->keyValueFactory = $keyValueFactory;
+    }
+
     public function getRoar($length)
     {
-        return 'R' . str_repeat('O', $length) . 'AR!';
+        $key = 'roar_' . $length;
+
+        $store = $this->keyValueFactory->get('dino');
+
+        if($store->has($key)){
+            return $store->get($key);
+        }
+
+        //pretend to be a delay in a query or something like that
+        sleep(2);
+
+        $string = 'R' . str_repeat('O', $length) . 'AR!';
+        $store->set($key, $string);
+
+        return $string;
     }
 }
