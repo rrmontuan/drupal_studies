159eb4f9020b09635ac4aeffe5eff5db8ac61b87
Ricardo Montuan <ricardo.montuan@hotmail.com>
2019-06-18 16:54:10 -0300

    chore: Added the roar service in roar controller.

--- app/modules/custom/dino_roar/src/Controller/RoarController.php
+++ app/modules/custom/dino_roar/src/Controller/RoarController.php
@@ -4,14 +4,29 @@ namespace Drupal\dino_roar\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Drupal\dino_roar\Jurassic\RoarGenerator;
+use Drupal\Core\Controller\ControllerBase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
-class RoarController
+class RoarController extends ControllerBase
 {
+    private $roarGenerator;
+
+    public function __construct(RoarGenerator $roarGenerator)
+    {
+        $this->roarGenerator = $roarGenerator;
+    }
+
     public function roar($count)
     {
-        $roarGenerator = new RoarGenerator;
-        $roar = $roarGenerator->getRoar($count);
+        $roar = $this->roarGenerator->getRoar($count);
 
         return new Response($roar);
+    }
+
+    public static function create(ContainerInterface $container)
+    {
+        $roarGenerator = $container->get('dino_roar.roar_generator');
+
+        return new static($roarGenerator);
     }
 }
